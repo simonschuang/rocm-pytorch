08d142b57e5a2b4e1b823d33c0a4b27d2a3b0cec
Xinya Zhang <Xinya.Zhang@amd.com>
2024-02-12 23:36:09 +0000

    test/test_transformers: skip test_flash_autocast_fp32_bfloat16/float16
    AOTriton V2's first kernel does not handle smaller block sizes.

--- test/test_transformers.py
+++ test/test_transformers.py
@@ -1510,6 +1510,7 @@ class TestSDPAFailureModes(NNTestCase):
                     q, k, v, None, 0.0, False))
 
     @onlyCUDA
+    @skipIfRocm  # Sequence length < 128
     @unittest.skipIf(not PLATFORM_SUPPORTS_FLASH_ATTENTION, "Does not support SDPA or pre-SM80 hardware")
     def test_flash_autocast_fp32_float16(self, device):
         dtype = torch.float
@@ -1522,6 +1523,7 @@ class TestSDPAFailureModes(NNTestCase):
                     q, k, v, None, 0.0, False)
 
     @onlyCUDA
+    @skipIfRocm  # Sequence length < 128
     @unittest.skipIf(not PLATFORM_SUPPORTS_FLASH_ATTENTION, "Does not support SDPA or pre-SM80 hardware")
     def test_flash_autocast_fp32_bfloat16(self, device):
         dtype = torch.float
